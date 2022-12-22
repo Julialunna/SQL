453dfc45bf3f4d6aa402987755baf0d0f0c86c57
Júlia Luna <85693548+Julialunna@users.noreply.github.com>
2022-12-22 14:15:04 -0300

    aprofundando em where

---
+++ uso_do_where.sql
@@ -0,0 +1,5 @@
+SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
+
+SELECT * FROM tbvendedores WHERE NOME='Claudia Morais';
+
+UPDATE tbprodutos SET SABOR = 'Cítricos' where SABOR = 'Limão';
